02a4b5bc33cbbedd8a6467ae360cc5d3ae3ea276
Kenan <kenan23@gmail.com>
2022-06-03 16:20:52 +0300

    Create django project and add Django products app.

---
+++ admin/products/views.py
@@ -0,0 +1,49 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework import status
+
+from .models import Product, User
+from .serializers import ProductSerializer
+
+import random
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        products = Product.objects.get(id=pk)
+        serializer = ProductSerializer(products)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        products = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=products, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.id,
+        })
